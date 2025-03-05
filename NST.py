937b86e3b4acca329e50cb175f1456877b119c18
Rushil Nalekar <r.m.nalekar@student.vu.nl>
2025-03-05 16:18:34 +0100

    Updated file

--- NST.py
+++ NST.py
@@ -7,202 +7,126 @@ from torch.optim import LBFGS
 import os
 from models.definitions.vgg19 import Vgg19
 
+# Constants for normalization
 IMAGENET_MEAN_255 = [123.675, 116.28, 103.53]
 IMAGENET_STD_NEUTRAL = [1, 1, 1]
 
-def load_image(img_path,target_shape="None"):
-    '''
-    Load and resize the image.
-    '''
+# Load image from path
+def load_image(img_path, target_shape=None):
     if not os.path.exists(img_path):
         raise Exception(f'Path not found: {img_path}')
-    img = cv.imread(img_path)[:, :, ::-1]                   # convert BGR to RGB when reading
-    if target_shape is not None:
-        if isinstance(target_shape, int) and target_shape != -1:
-            current_height, current_width = img.shape[:2]
-            new_height = target_shape
-            new_width = int(current_width * (new_height / current_height))
-            img = cv.resize(img, (new_width, new_height), interpolation=cv.INTER_CUBIC)
-        else:
-            img = cv.resize(img, (target_shape[1], target_shape[0]), interpolation=cv.INTER_CUBIC)
-    img = img.astype(np.float32)
-    img /= 255.0
+    img = cv.imread(img_path)[:, :, ::-1]  # Convert BGR to RGB
+    if target_shape:
+        img = cv.resize(img, (target_shape, target_shape), interpolation=cv.INTER_CUBIC)
+    img = img.astype(np.float32) / 255.0
     return img
 
+# Prepare image tensor for NST
 def prepare_img(img_path, target_shape, device):
-    '''
-    Normalize the image.
-    '''
-    img = load_image(img_path, target_shape=target_shape)
+    img = load_image(img_path, target_shape)
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.mul(255)),
-        transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)])
-    img = transform(img).to(device).unsqueeze(0)
-    return img
+        transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)
+    ])
+    return transform(img).to(device).unsqueeze(0)
 
-def save_image(img, img_path):
-    if len(img.shape) == 2:
-        img = np.stack((img,) * 3, axis=-1)
-    cv.imwrite(img_path, img[:, :, ::-1])                   # convert RGB to BGR while writing
+# Save tensor image to disk
+def save_image(img_tensor, img_path):
+    img = np.moveaxis(img_tensor.squeeze(0).cpu().detach().numpy(), 0, 2)
+    # Denormalize the image
+    img = np.clip(img + np.array(IMAGENET_MEAN_255).reshape((1, 1, 3)), 0, 255).astype('uint8')
+    cv.imwrite(img_path, img[:, :, ::-1])  # Convert RGB back to BGR
 
-def generate_out_img_name(config):
-    '''
-    Generate a name for the output image.
-    Example: 'c1-s1.jpg'
-    where c1: content_img_name, and
-          s1: style_img_name.
-    '''
-    prefix = os.path.basename(config['content_img_name']).split('.')[0] + '_' + os.path.basename(config['style_img_name']).split('.')[0]
-    suffix = f'{config["img_format"][1]}'
-    return prefix + suffix
-
-def save_and_maybe_display(optimizing_img, dump_path, config, img_id, num_of_iterations):
-    '''
-    Save the generated image.
-    If saving_freq == -1, only the final output image will be saved.
-    Else, intermediate images can be saved too.
-    '''
-    saving_freq = -1
-    out_img = optimizing_img.squeeze(axis=0).to('cpu').detach().numpy()
-    out_img = np.moveaxis(out_img, 0, 2)
-
-    if img_id == num_of_iterations-1 :
-        img_format = config['img_format']
-        out_img_name = str(img_id).zfill(img_format[0]) + img_format[1] if saving_freq != -1 else generate_out_img_name(config)
-        dump_img = np.copy(out_img)
-        dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
-        dump_img = np.clip(dump_img, 0, 255).astype('uint8')
-        cv.imwrite(os.path.join(dump_path, out_img_name), dump_img[:, :, ::-1])
-    
-
+# Load the VGG19 model for feature extraction
 def prepare_model(device):
-    '''
-    Load VGG19 model into local cache.
-    '''
     model = Vgg19(requires_grad=False, show_progress=True)
-    content_feature_maps_index = model.content_feature_maps_index
-    style_feature_maps_indices = model.style_feature_maps_indices
-    layer_names = model.layer_names
-    content_fms_index_name = (content_feature_maps_index, layer_names[content_feature_maps_index])
-    style_fms_indices_names = (style_feature_maps_indices, layer_names)
-    return model.to(device).eval(), content_fms_index_name, style_fms_indices_names
+    return model.to(device).eval(), model.content_feature_maps_index, model.style_feature_maps_indices
 
-def gram_matrix(x, should_normalize=True):
-    '''
-    Generate gram matrices of the representations of content and style images.
-    '''
+# Compute Gram matrix for style representation
+def gram_matrix(x):
     (b, ch, h, w) = x.size()
     features = x.view(b, ch, w * h)
-    features_t = features.transpose(1, 2)
-    gram = features.bmm(features_t)
-    if should_normalize:
-        gram /= ch * h * w
-    return gram
+    return features.bmm(features.transpose(1, 2)) / (ch * h * w)
 
-def total_variation(y):
-    '''
-    Calculate total variation.
-    '''
-    return torch.sum(torch.abs(y[:, :, :, :-1] - y[:, :, :, 1:])) + torch.sum(torch.abs(y[:, :, :-1, :] - y[:, :, 1:, :]))
+# Compute content and style loss
+def build_loss(neural_net, optimizing_img, target_representations, content_index, style_indices, config):
+    target_content = target_representations[0]
+    target_style = target_representations[1]
+    current_features = neural_net(optimizing_img)
+    # Content loss: MSE between target content and current content features
+    content_loss = torch.nn.MSELoss()(target_content, current_features[content_index].squeeze(0))
+    # Style loss: MSE between target style (Gram matrices) and current style Gram matrices
+    style_loss = 0
+    current_style = [gram_matrix(x) for cnt, x in enumerate(current_features) if cnt in style_indices]
+    for gt, cur in zip(target_style, current_style):
+        style_loss += torch.nn.MSELoss()(gt, cur)
+    total_loss = config['content_weight'] * content_loss + config['style_weight'] * style_loss
+    return total_loss
 
-def build_loss(neural_net, optimizing_img, target_representations, content_feature_maps_index, style_feature_maps_indices, config):
-    '''
-    Calculate content_loss, style_loss, and total_variation_loss.
-    '''
-    target_content_representation = target_representations[0]
-    target_style_representation = target_representations[1]
-    current_set_of_feature_maps = neural_net(optimizing_img)
-    current_content_representation = current_set_of_feature_maps[content_feature_maps_index].squeeze(axis=0)
-    content_loss = torch.nn.MSELoss(reduction='mean')(target_content_representation, current_content_representation)
-    style_loss = 0.0
-    current_style_representation = [gram_matrix(x) for cnt, x in enumerate(current_set_of_feature_maps) if cnt in style_feature_maps_indices]
-    for gram_gt, gram_hat in zip(target_style_representation, current_style_representation):
-        style_loss += torch.nn.MSELoss(reduction='sum')(gram_gt[0], gram_hat[0])
-    style_loss /= len(target_style_representation)
-    tv_loss = total_variation(optimizing_img)
-    total_loss = config['content_weight'] * content_loss + config['style_weight'] * style_loss + config['tv_weight'] * tv_loss
-    return total_loss, content_loss, style_loss, tv_loss
-
-def make_tuning_step(neural_net, optimizer, target_representations, content_feature_maps_index, style_feature_maps_indices, config):
-    '''
-    Performs a step in the tuning loop.
-    (We are tuning only the pixels, not the weights.)
-    '''
-    def tuning_step(optimizing_img):
-        total_loss, content_loss, style_loss, tv_loss = build_loss(neural_net, optimizing_img, target_representations, content_feature_maps_index, style_feature_maps_indices, config)
-        total_loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        return total_loss, content_loss, style_loss, tv_loss
-    return tuning_step
-
+# Main NST function
 def neural_style_transfer(config):
-    '''
-    The main Neural Style Transfer method.
-    '''
+    # Build full paths for content and style images
     content_img_path = os.path.join(config['content_images_dir'], config['content_img_name'])
     style_img_path = os.path.join(config['style_images_dir'], config['style_img_name'])
-    out_dir_name = 'combined_' + os.path.split(content_img_path)[1].split('.')[0] + '_' + os.path.split(style_img_path)[1].split('.')[0]
-    dump_path = os.path.join(config['output_img_dir'], out_dir_name)
-    os.makedirs(dump_path, exist_ok=True)
+    # Output file path (final combined image)
+    output_file = os.path.join(config['output_img_dir'],
+                               f"combined_{config['content_img_name'].split('.')[0]}_{config['style_img_name'].split('.')[0]}.jpg")
+    
+    os.makedirs(config['output_img_dir'], exist_ok=True)
+    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     content_img = prepare_img(content_img_path, config['height'], device)
     style_img = prepare_img(style_img_path, config['height'], device)
     
-    init_img = content_img
+    # Initialize optimizing image as a clone of the content image
+    optimizing_img = Variable(content_img.clone(), requires_grad=True)
     
-    optimizing_img = Variable(init_img, requires_grad=True)
-    neural_net, content_feature_maps_index_name, style_feature_maps_indices_names = prepare_model(device)
-    print(f'Using VGG19 in the optimization procedure.')
-    content_img_set_of_feature_maps = neural_net(content_img)
-    style_img_set_of_feature_maps = neural_net(style_img)
-    target_content_representation = content_img_set_of_feature_maps[content_feature_maps_index_name[0]].squeeze(axis=0)
-    target_style_representation = [gram_matrix(x) for cnt, x in enumerate(style_img_set_of_feature_maps) if cnt in style_feature_maps_indices_names[0]]
-    target_representations = [target_content_representation, target_style_representation]
-    num_of_iterations = 1000
+    neural_net, content_index, style_indices = prepare_model(device)
     
-    optimizer = LBFGS((optimizing_img,), max_iter=num_of_iterations, line_search_fn='strong_wolfe')
+    # Get target representations
+    target_content = neural_net(content_img)[content_index].squeeze(0)
+    # Compute Gram matrices for the style image features
+    target_style = [gram_matrix(x) for cnt, x in enumerate(neural_net(style_img)) if cnt in style_indices]
+    target_representations = [target_content, target_style]
+    
+    optimizer = LBFGS([optimizing_img], max_iter=config['num_of_iterations'], line_search_fn='strong_wolfe')
     cnt = 0
 
+    # Closure function for LBFGS optimization
     def closure():
         nonlocal cnt
         if torch.is_grad_enabled():
             optimizer.zero_grad()
-        total_loss, content_loss, style_loss, tv_loss = build_loss(neural_net, optimizing_img, target_representations, content_feature_maps_index_name[0], style_feature_maps_indices_names[0], config)
-        if total_loss.requires_grad:
-            total_loss.backward()
-        with torch.no_grad():
-            print(f'L-BFGS | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, tv loss={config["tv_weight"] * tv_loss.item():12.4f}')
-            save_and_maybe_display(optimizing_img, dump_path, config, cnt, num_of_iterations)
+        total_loss = build_loss(neural_net, optimizing_img, target_representations, content_index, style_indices, config)
+        total_loss.backward()
+        print(f'Iteration {cnt:03}, Total Loss: {total_loss.item():.4f}')
+        # Save image every 50 iterations
+        if cnt % 50 == 0:
+            save_image(optimizing_img, output_file)
+            print(f"Image saved at iteration {cnt}: {output_file}")
         cnt += 1
         return total_loss
+
     optimizer.step(closure)
-    return dump_path
+    # Save final image
+    save_image(optimizing_img, output_file)
+    print(f"Final image saved: {output_file}")
+    return output_file
 
+# Configuration
 PATH = r"C:\Users\rushi_a87oqn1\Desktop\Neural-Style-Transfer\data"
-
-CONTENT_IMAGE = "c5.jpg"  
-STYLE_IMAGE = "s2.jpg"    
-
-content_images_dir = os.path.join(PATH, 'content-images')
-style_images_dir = os.path.join(PATH, 'style-images')
-output_img_dir = os.path.join(PATH, 'output-images')
-
-img_format = (4, '.jpg')
-
 optimization_config = {
-    'content_img_name': CONTENT_IMAGE,
-    'style_img_name': STYLE_IMAGE,
+    'content_img_name': "c1.jpg",
+    'style_img_name': "s1.jpg",
     'height': 400,
-    'content_weight': 1000.0,
-    'style_weight': 10000.0,
-    'tv_weight': 1.0,
-    'content_images_dir': content_images_dir,
-    'style_images_dir': style_images_dir,
-    'output_img_dir': output_img_dir,
-    'img_format': img_format
+    'content_weight': 10000.0,
+    'style_weight': 1000.0,
+    'num_of_iterations': 500,  # You can adjust this number
+    'content_images_dir': os.path.join(PATH, 'content-images'),
+    'style_images_dir': os.path.join(PATH, 'style-images'),
+    'output_img_dir': os.path.join(PATH, 'output-images')
 }
 
+# Run NST
 results_path = neural_style_transfer(optimization_config)
-
